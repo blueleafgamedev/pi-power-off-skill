310e793fc8805971a93ea5bc739e97a719a158f3
Travis Engh <engh.travis@outlook.com>
2022-02-20 20:16:21 -0600

    Add reboot and reset

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+from subprocess import call
 
 class PiPowerOff(MycroftSkill):
     def __init__(self):
@@ -8,6 +8,18 @@ class PiPowerOff(MycroftSkill):
     @intent_file_handler('off.power.pi.intent')
     def handle_off_power_pi(self, message):
         self.speak_dialog('off.power.pi')
+        call('sudo /sbin/shutdown', shell=True)
+
+    @intent_file_handler('reboot.power.pi.intent')
+    def handle_reboot_power_pi(self, message):
+        self.speak_dialog('reboot.power.pi')
+        call('sudo /sbin/reboot', shell=True)
+
+    @intent_file_handler('reset.power.pi.intent')
+    def handle_reset_dialog(self, message):
+        self.speak_dialog('reset.power.pi')
+        call('bash /home/tengh/mycroft-core/start-mycroft.sh all restart', shell=True)
+
 
 
 def create_skill():
